780c634ae164ef2af240e82d8023b1e69525de26
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-11-06 13:07:53 -0700

    Created socket; more code yet to be filled in

--- WebServer.py
+++ WebServer.py
@@ -4,15 +4,27 @@ import sys # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#Prepare a sever socket
-#TODO
+# Prepare a server socket
+#SERVER_HOST = "localhost"
+SERVER_HOST = "127.0.0.1"	# Host address; localhost
+SERVER_PORT = 8080			# Listening at
+
+# Not sure how this line works exactly
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Bind the socket to address
+# A pair (host, port) is used for the AF_INET address family
+serverSocket.bind((SERVER_HOST, SERVER_PORT))
+
+serverSocket.listen(1)
 
 while True:
 	# Establish the connection
 	print('Ready to serve...')
-	connectionSocket, addr = #TODO
+	clientSocket, clientAddr = serverSocket.accept() #TODO
+	
 	try:
-		message = #TODO
+		message = "HelloWorld.html"
 		filename = message.split()[1]
 		f = open(filename[1:])
 		outputdata = #TODO
@@ -20,18 +32,18 @@ while True:
 		# Send one HTTP header line into socket
 		#TODO
 
-		#Send the content of the requested file to the client
+		# Send the content of the requested file to the client
 		for i in range(0, len(outputdata)):
-		connectionSocket.send(outputdata[i].encode())
-		connectionSocket.send("\r\n".encode())
+			clientSocket.send(outputdata[i].encode())
+		clientSocket.send("\r\n".encode())
 
 		connectionSocket.close()
 	except IOError:
-		#Send response message for file not found
+		# Send response message for file not found
 		#TODO
 
-		 #Close client socket
-		 #TODO
+		# Close client socket
+		#TODO
 
 serverSocket.close()
 sys.exit() # Terminate the program after sending the corresponding data 
